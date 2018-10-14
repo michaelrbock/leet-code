93fe0aec3c3401efedae183cbf31530da0d2b57b
michaelrbock <mykel.bock@gmail.com>
2018-10-14 15:15:46 -0700

    Lints triangle.py

--- python/triangle.py
+++ python/triangle.py
@@ -1,8 +1,8 @@
-"""
-Dynamic Programming.
-"""
+"""Dynamic Programming."""
+
 
 class Solution:
+
   def minimumTotal(self, triangle):
     """
     :type triangle: List[List[int]]
@@ -16,20 +16,23 @@ class Solution:
         continue
       next_level = []
       for j, elem in enumerate(row):
-        next_level.append(min(min_paths[j], min_paths[j+1]) + elem)
+        next_level.append(min(min_paths[j], min_paths[j + 1]) + elem)
       min_paths = next_level
     return min_paths[0]
 
 
 class Solution1:
   """Doesn't pass OJ's Time Limit."""
+
   def minimum_total_rec(self, triangle, memo, row, spot):
     if row == len(triangle) - 1:
       return triangle[row][spot]
-    left = memo.get((row + 1, spot), self.minimum_total_rec(triangle, memo, row + 1, spot))
+    left = memo.get((row + 1, spot),
+                    self.minimum_total_rec(triangle, memo, row + 1, spot))
     if (row + 1, spot) not in memo:
       memo[(row + 1, spot)] = left
-    right = memo.get((row + 1, spot + 1), self.minimum_total_rec(triangle, memo, row + 1, spot + 1))
+    right = memo.get((row + 1, spot + 1),
+                     self.minimum_total_rec(triangle, memo, row + 1, spot + 1))
     if (row + 1, spot + 1) not in memo:
       memo[(row + 1, spot + 1)] = right
     return triangle[row][spot] + min(left, right)
