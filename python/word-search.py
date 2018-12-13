58a162d8ce789990c41974d951f0b2fe77c0d80e
michaelrbock <mykel.bock@gmail.com>
2018-12-13 08:21:05 -0800

    updates

--- python/word-search.py
+++ python/word-search.py
@@ -1,31 +1,22 @@
-"""
-Backtracking.
-"""
+OFFSETS = [(-1, 0), (0, -1), (1, 0), (0, 1)]
 
 
-_DIRECTIONS = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+def dfs(board, word, visited, row, col):
+  if len(word) == 1:
+    return True
+  word = word[1:]
+  visited.add((row, col))
+  for r_offset, c_offset in OFFSETS:
+    new_row, new_col = row + r_offset, col + c_offset
+    if 0 <= new_row < len(board) and 0 <= new_col < len(board[0]):
+      if board[new_row][new_col] == word[0] and (new_row, new_col) not in visited:
+        if dfs(board, word, visited, new_row, new_col):
+          return True
+  visited.remove((row, col))
+  return False
 
 
 class Solution:
-
-  def exist_rec(self, board, word, row, col, visited):
-    if len(word) == 1:
-      return True
-    word = word[1:]
-    visited.add((row, col))
-    for i, j in _DIRECTIONS:
-      new_row = row + i
-      new_col = col + j
-      if new_row < 0 or new_row >= len(board) or new_col < 0 or new_col >= len(
-          board[0]):
-        continue
-      if board[new_row][new_col] == word[0] and (new_row,
-                                                 new_col) not in visited:
-        if self.exist_rec(board, word, new_row, new_col, visited):
-          return True
-    visited.remove((row, col))
-    return False
-
   def exist(self, board, word):
     """
     :type board: List[List[str]]
@@ -35,9 +26,10 @@ class Solution:
     if len(board) * len(board[0]) < len(word):
       return False
 
+    # Strategy: DFS w/ backtracking starting from each letter that == word[0].
     for row in range(len(board)):
       for col in range(len(board[0])):
         if board[row][col] == word[0]:
-          if self.exist_rec(board, word, row, col, set()):
+          if dfs(board, word, set(), row, col):
             return True
     return False
