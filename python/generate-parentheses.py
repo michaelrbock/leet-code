9616a9a3b76083f9560f77a93bb1fba0b02c6602
michaelrbock <mykel.bock@gmail.com>
2018-12-15 11:05:06 -0800

    Updates gen parens

--- python/generate-parentheses.py
+++ python/generate-parentheses.py
@@ -1,22 +1,24 @@
-"""
-Backtracking/Recursion.
-"""
-
-def generate_parens_rec(so_far, opens, closes):
-  if opens == 0 and closes == 0:
-    return [so_far]
-  results = []
-  if opens > 0:
-    results.extend(generate_parens_rec(so_far + '(', opens - 1, closes))
-  if opens < closes:
-    results.extend(generate_parens_rec(so_far + ')', opens, closes - 1))
-  return results
-
-
 class Solution:
   def generateParenthesis(self, n):
     """
     :type n: int
     :rtype: List[str]
     """
-    return generate_parens_rec('(', n - 1, n)
+    # Strategy: recursive DFS over a tree where you can either place
+    #   a left or right paren, pruning when we've created an un-balanced
+    #   string.
+    results = []
+
+    def parens_rec(to_place, so_far, left_parens):
+      if left_parens < 0:
+        return  # prune tree.
+
+      if to_place == 0:
+        if left_parens == 0:
+          results.append(so_far)
+        return
+      parens_rec(to_place - 1, so_far + '(', left_parens + 1)
+      parens_rec(to_place - 1, so_far + ')', left_parens - 1)
+
+    parens_rec(n * 2, '', 0)
+    return results
