422b879640862ac10975732af4d0a76df3416d17
Michael Bock <mykel.bock@gmail.com>
2019-09-24 18:09:51 -0700

    Add additional 98 solution

--- python/validate-binary-search-tree.py
+++ python/validate-binary-search-tree.py
@@ -21,3 +21,31 @@ class Solution:
         is_bst_rec(node.right, node.val, lt))
 
     return is_bst_rec(root, float('-inf'), float('inf'))
+
+
+from collections import deque
+
+
+class Solution2(object):
+  def isValidBST(self, root):
+    """
+    :type root: TreeNode
+    :rtype: bool
+    """
+    if not root:
+      return True
+
+    queue = deque([(root, float('-inf'), float('inf'))])
+
+    while queue:
+      node, greater_than, less_than = queue.popleft()
+      if node.val <= greater_than or node.val >= less_than:
+        return False
+
+      if node.left:
+        queue.append((node.left, greater_than, node.val))
+
+      if node.right:
+        queue.append((node.right, node.val, less_than))
+
+    return True
